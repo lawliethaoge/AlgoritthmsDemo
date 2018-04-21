1b0171386ffdf31bf73f91da7c916aa4bd9faf4f
lawliet <653772348@qq.com>
2018-04-21 17:07:33 +0800

    加入希尔排序

--- src/com/lawliethaoge/sort/Insertion.java
+++ src/com/lawliethaoge/sort/Insertion.java
@@ -1,8 +1,36 @@
 package com.lawliethaoge.sort;
 
+
+import java.util.Scanner;
+
 /**
+ * 插入排序
  * @author hao@lawliet.com
  * @since 2018/4/20 20:15
  */
 public class Insertion {
+    public  static void sort(long[] a){
+       long tmp = 0;
+       for(int i=1;i<a.length;i++){
+           for(int j=i;j>0 && a[j]<a[j-1];j--){  //将数组右移
+               tmp=a[i];
+               a[j] = a[j-1];
+               a[j-1] =tmp;
+           }
+
+       }
+    }
+
+    public static void main(String[] args) {
+        long[] a = {1,5,2,67,31,53,674};
+        sort(a);
+
+        for(long b: a){
+            System.out.print(b+",");
+        }
+
+
+    }
+
+
 }
